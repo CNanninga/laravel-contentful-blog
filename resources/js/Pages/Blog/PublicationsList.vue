e7be7250c57f09e64262fd77d30738b0bc9d7363
Chris Nanninga <tarsusc81@gmail.com>
2023-07-08 16:40:06 -0500

    Formatting for off-site links

--- resources/js/Pages/Blog/PublicationsList.vue
+++ resources/js/Pages/Blog/PublicationsList.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Base from '@/Components/Blog/Base.vue';
 import ListNav from '@/Components/Blog/ListNav.vue';
-import { Link } from '@inertiajs/vue3';
+import { ArrowTopRightOnSquareIcon } from '@heroicons/vue/24/outline';
 
 const props = defineProps([
     'copyrightYear',
@@ -21,7 +21,8 @@ const props = defineProps([
             <article v-for="publication in publications" :key="publication.title" class="mb-8 px-8 md:px-0">
                 <header>
                     <h2>
-                        <Link :href="publication.url">{{ publication.title }}</Link>
+                        <a :href="publication.url" target="_blank" class="leading-8 align-middle">{{ publication.title }}
+                            <ArrowTopRightOnSquareIcon class="h-5 w-5 inline-block leading-8 align-middle" /></a>
                     </h2>
                     <div class="bg-slate-500 inline-block text-sm text-white py-1 px-2 rounded-md font-bold">
                         {{ publication.type }}
